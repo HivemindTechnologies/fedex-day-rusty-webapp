f798927d401bc907965ea9c9bfa5cba1cfef2151
Jan Klenter <jan.klenter@hivemindtechnologies.com>
2023-05-05 12:38:41 +0200

    remove comment

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,6 @@ async fn main() {
         .route("/joke", post(joke));
 
     // run our app with hyper, listening globally on port 3000
-    //    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
